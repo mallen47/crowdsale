8eed57c05be264efefdc4a9e29df903ae2b90981
Matt Allen <allen.matt@gmail.com>
2024-12-29 15:54:17 -0500

    connect react app to meta mask

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,28 @@
+import { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
+import { ethers } from 'ethers';
+
 import Navigation from './Navigation';
+import Info from './Info';
 
 function App() {
+  const [account, setAccount] = useState(null);
+  const loadBlockChainData = async () => {
+    //const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    const account = ethers.utils.getAddress(accounts[0]);
+    setAccount(account);
+  };
+
+  useEffect(() => {
+    loadBlockChainData();
+  });
+
   return (
     <Container>
       <Navigation />
+      <hr />
+      {account && <Info account={account} />}
     </Container>
   );
 }
